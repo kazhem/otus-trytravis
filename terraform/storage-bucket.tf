c0b9c354aa9b58c8829a182d845b61dc899e2b05
Mikhail <Kazhemski>
2020-04-11 15:36:14 +0300

    Add external module storage

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,15 @@
+provider "google" {
+    version = "~> 2.15"
+    project = var.project
+    region = var.region
+}
+module "storage-bucket" {
+    source = "SweetOps/storage-bucket/google"
+    version = "0.3.1"
+    # Имена поменяйте на другие
+    name = "kazhemm-otus-tf-storage"
+    location    = var.region
+    }
+output storage-bucket_url {
+    value = module.storage-bucket.url
+}
